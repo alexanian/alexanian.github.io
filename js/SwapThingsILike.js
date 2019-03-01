d8fc7b3941a97206c26c833ed524d03ff615fa20
Tessa <symbiologer@gmail.com>
2019-02-28 20:53:01 -0800

    What if more colours?

--- js/SwapThingsILike.js
+++ js/SwapThingsILike.js
@@ -1,8 +1,13 @@
 var SwapThingsILike = {
     // Used for swapping through text in header
     idx: 0,
-    things: ["Too Many Things", "Synthetic Biology", "Strange Objects", "Event Logistics",
-        "Communication", "Miscellaneous Skills"],
+    things: [
+        'Too Many Things',
+        '<span class="category-biology">Synthetic Biology</span>',
+        '<span class="category-making">Strange Objects</span>',
+        '<span class="category-events">Event Logistics</span>',
+        '<span class="category-comms">Communication</span>',
+        '<span class="category-misc">Miscellaneous Skills</span>'],
     
     init: function () {
         SwapThingsILike.bindUI();
